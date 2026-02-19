939813e384f2a07cd391b85988829d3d393f031b
Oliver Steiner <github@jsemolik.dev>
2026-02-19 22:52:44 +0100

    some nabar changes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -27,32 +27,34 @@ export function Navbar() {
   }
 
   return (
-    <header className="sticky top-0 z-50 flex justify-end items-center px-4 gap-4 h-16 w-full border-b border-white bg-background/80 backdrop-blur-sm">
-      <ClerkLoading>
-        <div className="flex items-center gap-4 h-full">
-          <Skeleton className="h-8 w-16" />
-          <Skeleton className="h-full w-28 rounded-none" />
-        </div>
-      </ClerkLoading>
-      <ClerkLoaded>
-        <SignedOut>
-          <div className="flex items-center gap-4">
-            <Link href="/sign-in">
-              <Button variant="ghostCta" className="text-md px-2">
-                Sign In
+    <header className="sticky top-0 z-50 flex justify-end items-center h-16 w-full border-b border-white bg-background/80 backdrop-blur-sm">
+      <div className="flex items-center gap-4 pl-4 h-full">
+        <ClerkLoading>
+          <div className="flex items-center gap-4 h-full">
+            <Skeleton className="h-8 w-16" />
+            <Skeleton className="h-full w-28 rounded-none" />
+          </div>
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedOut>
+            <div className="flex items-center gap-4">
+              <Link href="/sign-in">
+                <Button variant="ghostCta" className="text-md px-2">
+                  Sign In
+                </Button>
+              </Link>
+            </div>
+            <Link href="/sign-up" className="h-full self-stretch flex">
+              <Button variant="cta" className="h-full border-y-0 p-6 text-md">
+                Get Started
               </Button>
             </Link>
-          </div>
-          <Link href="/sign-up" className="h-full self-stretch flex">
-            <Button variant="cta" className="h-full border-y-0 p-6 text-md">
-              Get Started
-            </Button>
-          </Link>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </ClerkLoaded>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </ClerkLoaded>
+      </div>
       <ColorToggle />
     </header>
   );
