d453fd04ae02b8b8a8ea1b5d25d01756a7f1ae66
JsemOlik <github@jsemolik.dev>
2026-02-19 15:47:02 +0100

    Sticky navbar and sign in page

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { SignIn, useUser } from "@clerk/nextjs";
+
+export default function Home() {
+  const { isSignedIn } = useUser();
+
+  if (!isSignedIn) {
+    return <SignIn />;
+  }
+
+  return <div>Welcome!</div>;
+}
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,23 +1,18 @@
 "use client";
 
-import {
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { ColorToggle } from "@/components/color-toggle";
+import Link from "next/link";
 
 export function Navbar() {
   return (
     <header className="sticky top-0 z-50 flex justify-end items-center p-4 gap-4 h-16 w-full border-b border-white bg-background/80 backdrop-blur-sm">
       <SignedOut>
-        <SignInButton mode="modal" />
-        <SignUpButton mode="modal">
+        <Link href="/sign-in">Sign In</Link>
+        <Link href="/sign-up">
           <Button variant="cta">Sign Up</Button>
-        </SignUpButton>
+        </Link>
       </SignedOut>
       <SignedIn>
         <UserButton />
